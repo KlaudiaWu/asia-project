8a5bb14c81e724d6335b40d67a2a426c8fd30cab
Klaudia Wójciak <klaudys1607@gmail.com>
2018-08-03 19:29:20 +0200

    Initial version nav, pictures in contakt section

---
+++ menu_navigation.js
@@ -0,0 +1,10 @@
+
+function openMenu() {
+    document.getElementById("menu_navigation").style.width = "25vw";
+    document.getElementById("overlay").backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeMenu() {
+    document.getElementById("menu_navigation").style.width = "0";
+    document.getElementById("overlay").backgroundColor = "transparent";
+}
