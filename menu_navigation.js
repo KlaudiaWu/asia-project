a95f1d39421512cef04062461084390c651cb7b5
Klaudia Wójciak <klaudys1607@gmail.com>
2018-08-09 22:40:05 +0200

    Work on slider-portfolio in progress, finished video section

--- menu_navigation.js
+++ menu_navigation.js
@@ -8,19 +8,3 @@ function closeMenu() {
     document.getElementById("menu_navigation").style.width = "0";
     document.getElementById("overlay").backgroundColor = "transparent";
 }
-
-//enabling smooth scroll
-$(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });
