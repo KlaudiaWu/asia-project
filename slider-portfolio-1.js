0c64abe90fcb8d164ceb7b4dc47244e387a6d8c3
Klaudia Wójciak <klaudys1607@gmail.com>
2018-08-10 20:23:49 +0200

    Slider-portfolio: finished work on button "next" animation; "previous" button to write

---
+++ slider-portfolio-1.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+
+    var slides = $(".slide-portfolio");
+    var current_index = 0;
+    var last_index = slides.length - 1; //ilosc slajdow
+
+    var transition_event_set = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
+
+    var prev = $(".left-nav-portfolio");
+    var next = $(".right-nav-portfolio");
+    var inTransit = false;
+
+    function setup() {
+        var width_text = "";
+        var width_image = "";
+        slides.each(function (index) {
+            var that = $(this);
+            var text = that.find(".portfolio_text");
+            var image = that.find(".portfolio_image");
+            if (index == 0) {
+                that.addClass("active stay_relative");
+                width_text = text.width();
+                width_image = image.width();
+                firstElem = that;
+            } else {
+                that.addClass("inactive");
+            }
+            
+            text.width(width_text);
+            image.width(width_image);
+        });
+
+        next.on("click", nextSlide);
+    }
+
+    setup();
+
+    function getNextElement(that, index) {
+        if (index === last_index) { // ostatni element
+            return firstElem;
+        } else {
+            return that.next();
+        }
+    }
+
+    function nextSlide() {
+        $.each(slides, function (index, value) {
+            if (index === current_index) {
+                var that = $(this);
+                var nextElement = getNextElement(that, index);
+                
+                that.addClass("portfolio-animate-section");
+                nextElement.addClass("active").removeClass("inactive");
+
+                that.one(transition_event_set, function (e) {
+                    that.addClass("inactive").removeClass("active portfolio-animate-section stay_relative");
+                    nextElement.addClass("stay_relative");
+                });
+            }
+        });
+
+        current_index++;
+        if(current_index > last_index) {
+            current_index = 0;
+        }
+
+    }
+});
