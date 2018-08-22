25b58d917f3cda40448cb48ef5ff4223dbca55c6
Klaudia Wójciak <klaudys1607@gmail.com>
2018-08-22 19:12:49 +0200

    Added all media-queries for index page

--- js/menu_navigation.js
+++ js/menu_navigation.js
@@ -1,9 +1,11 @@
 
 function openMenu() {
-    if (window.matchMedia("(max-width: 600px)").matches) {
+    if (window.matchMedia("(orientation: portrait) and (max-width: 600px)").matches) {
         document.getElementById("menu-navigation").style.width = "100vw";
-    } else if (window.matchMedia("(min-width: 600px) and (max-width: 1280px)").matches) {
+    } else if (window.matchMedia("(orientation: portrait) and (min-width: 600px) and (max-width: 1280px)").matches) {
         document.getElementById("menu-navigation").style.width = "50vw";
+    } else if (window.matchMedia("(orientation: landscape) and (max-width: 1280px)").matches) {
+        document.getElementById("menu-navigation").style.width = "70vw";    
     } else {
         document.getElementById("menu-navigation").style.width = "25vw";
     }
