d11b3d05d11df7cf81afab7c9ba9056a4e2f18ea
Klaudia Wójciak <klaudys1607@gmail.com>
2018-08-22 13:13:30 +0200

    Created media queries up to 1200px in portraits mode

--- js/menu_navigation.js
+++ js/menu_navigation.js
@@ -1,6 +1,12 @@
 
 function openMenu() {
-    document.getElementById("menu-navigation").style.width = "25vw";
+    if (window.matchMedia("(max-width: 600px)").matches) {
+        document.getElementById("menu-navigation").style.width = "100vw";
+    } else if (window.matchMedia("(min-width: 600px) and (max-width: 1280px)").matches) {
+        document.getElementById("menu-navigation").style.width = "50vw";
+    } else {
+        document.getElementById("menu-navigation").style.width = "25vw";
+    }
 }
 
 function closeMenu() {
